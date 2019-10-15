ad115f075460c11b632e34fe48f76ee576d542f8
Stefano-t <stefano.taverni@stud.unifi.it>
2019-10-15 18:18:22 +0200

    Starting TestMainBehaviour refactoring

--- src/main/java/engine/MainBehaviour.java
+++ src/main/java/engine/MainBehaviour.java
@@ -1,6 +1,5 @@
 package engine;
 
-import java.util.Arrays;
 
 import exceptions.CharAbsenceException;
 import exceptions.IllegalCharException;
@@ -20,20 +19,15 @@ public class MainBehaviour {
 		this.ui = ui;
 	}
 
-	// TO DELETE
-	public MainBehaviour(MainExecutive executive2) {
-		this.executive = executive2;
-	}
-
 	public void executeControl(char c) {
 		try {
 			char[] guessingWord = executive.performCharControl(c);
-			System.out.println(Arrays.toString(guessingWord));
+			ui.printGuessingWord(guessingWord);
 		} catch (CharAbsenceException charAbs) {
 			errorCounter++;
-			System.out.println(charAbs.getMessage());
+			ui.printExceptionMessage(charAbs);
 		} catch (IllegalCharException illChar) {
-			System.out.println(illChar.getMessage());
+			ui.printExceptionMessage(illChar);
 		}
 	}
 
