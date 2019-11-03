aba380e75c7a92c85303f62da193ebd0ad7407b0
Stefano-t <stefano.taverni@stud.unifi.it>
2019-11-03 13:13:07 +0100

    Add other words

--- thehangman-app/src/main/resources/load.sql
+++ thehangman-app/src/main/resources/load.sql
@@ -1,1 +1,1 @@
-INSERT INTO Words (word_id, word_string) VALUES (1, "cane"), (2, "padrone"), (3, "fiera"), (4, "pallone"), (5, "autosufficiente");
+INSERT INTO Words (word_id, word_string) VALUES (1, "cane"), (2, "effimero"), (3, "fiera"), (4, "pallone"), (5, "aracnofobia"), (6, "java"), (7, "algoritmo"), (8, "linux");
